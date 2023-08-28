5933f90d3da8a20655213d67cd9226ffe5e4ac19
hoanganh <anhnh3@bsscommerce.com>
2023-08-28 11:38:02 +0700

    add container node:16

---
+++ 16/setups/users.sh
@@ -0,0 +1,7 @@
+[ ! -z "$TERM" -a -r ~/.motd ] && cat ~/.motd
+HISTTIMEFORMAT="%d/%m/%Y %T "
+alias ls='ls --color=auto'
+alias grep='grep --color=auto'
+alias fgrep='fgrep --color=auto'
+alias egrep='egrep --color=auto'
+alias diff='diff --color=auto'
